530b7b505df434ca3451978007fb682f3beec929
pbl.gllgs <pbl.gllgs@gmail.com>
2021-12-18 11:25:15 -0300

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+let miNuevoNombre = 'pablo '
+console.log(miNuevoNombre + 'gallegos')
+
+// alert('Hola desde app.js');
+// console.log('hola');
+
+let a = 10,
+    b = 'string',
+    c = "Spiderman ",
+    d = "Araña",
+    z = a + b;
+
+let arr = [1, 2, 3, 4, 5];
+
+const saludo = c + d;
+
+// console.log(saludo);
+
+// console.log('%c Consola','color:red')
+// console.log({a})
+// console.info(a)
+// console.warn(a)
+// console.error(a)
+
+// console.table({arr,a,b});
